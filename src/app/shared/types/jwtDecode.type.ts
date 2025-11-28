27671b35324695f2c2595be12f06e9d8f083f9fe
Bimal <bimal.devasia@geekywolf.com>
2025-11-28 15:20:45 +0530

    added auth and interceptor

---
+++ src/app/shared/types/jwtDecode.type.ts
@@ -0,0 +1,5 @@
+export interface jwtValues {
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": string;
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress": string;
+    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string;
+}

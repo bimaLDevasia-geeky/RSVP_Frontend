c0d5512ec354690c24ef08f57b9043586fd0d3fe
Bimal <bimal.devasia@geekywolf.com>
2025-12-04 11:19:00 +0530

    some changes

--- src/app/features/auth/pages/user-register/user-register.ts
+++ src/app/features/auth/pages/user-register/user-register.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthService } from   '../../../../core/auth/services/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { HotToastService } from '@ngxpert/hot-toast';
 
 
@@ -16,8 +16,8 @@ export class UserRegister {
   constructor(private authService: AuthService, private fb: FormBuilder,private router: Router,private toastservice: HotToastService) {}
 
   registerForm!: FormGroup;
-
-  
+  route=inject(ActivatedRoute)
+  returnUrl: string | null = this.route.snapshot.queryParamMap.get('return');
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -25,6 +25,7 @@ export class UserRegister {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+
   }
 
   onSubmit(): void {
@@ -33,7 +34,12 @@ export class UserRegister {
       this.authService.register(name, email, password).subscribe({
         next: (response) => {
           console.log('Registration successful', response);
+          if (this.returnUrl) {
+            this.router.navigate(['/login'], { queryParams: { return: this.returnUrl } });
+          } else {
           this.router.navigate(['/login']);
+          }
+          this.toastservice.success('Registration successful! Please log in.');
         },
         error: (error) => {
           this.toastservice.error('Registration failed.');
